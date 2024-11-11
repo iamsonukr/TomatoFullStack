8975c60bdcf74fd89d8ca2c24baca75060a9d9bb
Sonu Kumar <thesonukumar357@gmail.com>
2024-11-11 11:35:08 +0530

    Loaded

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import {assets} from '../../assets/assets'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img className='logo' src={assets.logo} alt="" />
+        <img className='logo'  src={assets.logo} alt="" />
         <img className='profile' src={assets.profile_image} alt="" />
     </div>
   )
